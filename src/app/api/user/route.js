4df3489663fab121d990f3842508b00e717e5359
shaikat17 <shaikatpal56@gmail.com>
2023-07-11 00:10:19 +0600

    User create and get all user api created

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -6,13 +6,13 @@ import { NextResponse } from "next/server";
 export const POST = async (request) => {
   const {name, email, photoUrl, phone_no, location} = await request.json()
   await connectMongoDB();
-  console.log(name, email, photoUrl, phone_no, location);
+  // console.log(name, email, photoUrl, phone_no, location);
+  await User.create({name, email, photoUrl, phone_no, location})
   // await Topic.create({title, description})
-  return NextResponse.json({ message: "Topic Created" }, { status: 201 });
+  return NextResponse.json({ message: "User Created." }, { status: 201 });
 };
 
 export const GET = async () => {
-  const users = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await users.json()
-  return NextResponse.json(data);
+  const users = await User.find({})
+  return NextResponse.json(users);
 };

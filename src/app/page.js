cbf6af36aea94232b7cb1f76577681617b64be74
shaikat17 <shaikatpal56@gmail.com>
2023-07-11 15:01:59 +0600

    Rearrange the page routes.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
-const Home = () => {
-    return (
-        <>
-        <h1>Hello</h1>
-        </>
-    )
-}
+import About from "@/components/Home/About/About";
+import Hero from "@/components/Home/Hero/Hero";
+import JoinUs from "@/components/Home/JoinUs/JoinUs";
+import Partners from "@/components/Home/Partners/Partners";
+import ProjectGallery from "@/components/Home/ProjectGallery/ProjectGallery";
+import Service from "@/components/Home/Service/Service";
+import Testimonial from "@/components/Home/Testimonial/Testimonial";
+import Volunteer from "@/components/Home/Volunteer/Volunteer";
 
-export default Home
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Service />
+      <Partners />
+      <ProjectGallery />
+      <Volunteer />
+      <Testimonial />
+      <JoinUs />
+    </>
+  );
+};
+
+export default Home;

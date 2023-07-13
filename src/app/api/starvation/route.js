5009410358d371bad2429a0cbd47a04da5d38072
shaikat17 <shaikatpal56@gmail.com>
2023-07-13 23:56:32 +0600

    Starvation data model and route created.

---
+++ src/app/api/starvation/route.js
@@ -0,0 +1,18 @@
+import connectMongoDB from "@/config/mongodb";
+import Starvation from "@/models/starvationModel";
+import { NextResponse } from "next/server";
+
+export const POST = async (request) => {
+  const {seeker_name, seeker_email, seeker_image, location, phone_no, deadline, amount, emergency, verification, reason, supermarket_name, supermarket_location, supermarket_contact_number} = await request.json()
+  await connectMongoDB();
+  // console.log(name, email, photoUrl, phone_no, location);
+  await Starvation.create({seeker_name, seeker_email, seeker_image, location, phone_no, deadline, amount, emergency, verification, reason, supermarket_name, supermarket_location, supermarket_contact_number})
+  // await Topic.create({title, description})
+  return NextResponse.json({ message: "Starvation Data Created." }, { status: 201 });
+};
+
+export const GET = async () => {
+  await connectMongoDB()
+  const starvation = await Starvation.find({})
+  return NextResponse.json(starvation);
+};
---
+++ src/models/starvationModel.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema } from "mongoose";
+
+const starvationSchema = new Schema(
+    {
+        seeker_name: {
+          type: String,
+        },
+        seeker_email: {
+          type: String,
+        },
+        seeker_image: {
+          type: String,
+        },
+        location: {
+            type: String,
+        },
+        phone_no: {
+            type: String,
+        },
+        deadline: {
+            type: String,
+        },
+        amount: {
+            type: String,
+        },
+        emergency: {
+            type: String,
+        },
+        verification: {
+            type: String,
+        },
+        reason: {
+            type: String,
+        },
+        supermarket_name: {
+            type: String,
+        },
+        supermarket_location: {
+            type: String,
+        },
+        supermarket_contact_number: {
+            type: String,
+        },
+      },
+      {
+        timestamps: true,
+      }
+)
+
+const Starvation = mongoose.models.Starvation || mongoose.model("Starvation", starvationSchema)
+
+export default Starvation;

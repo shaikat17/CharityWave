7bd7bdc33d3b38d93427f711872a6ad6b08e41d5
shaikat17 <shaikatpal56@gmail.com>
2023-07-17 22:26:06 +0600

    Deployment issue solved.

--- src/app/api/medical/new/route.js
+++ src/app/api/medical/new/route.js
@@ -1,8 +1,8 @@
 
 import connectMongoDB from "@/config/mongodb";
+import Medical from "@/models/medicalModel";
 import { ClientSession } from "mongodb";
 import { NextResponse } from "next/server";
-import Medical from "../../../models/medicalModel";
 
 export const POST = async (request) => {
   const {seeker_name, seeker_email, seeker_image, location, phone_no, deadline, amount, emergency, verification, reason, sub_category, hospital_name, hospital_location, hospital_contact_number, pharmacy_name, pharmacy_location, pharmacy_contact_number} = await request.json()

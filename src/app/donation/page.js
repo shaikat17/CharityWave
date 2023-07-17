fafaf8cb141d9fb3a1e9417474f307e6464eb025
shanin18 <112709243+shanin18@users.noreply.github.com>
2023-07-17 16:55:46 +0600

    added category functionality

--- src/app/donation/page.js
+++ src/app/donation/page.js
@@ -1,51 +1,57 @@
-'use client'
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
-import { HiFilter } from 'react-icons/hi';
+"use client";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { HiFilter } from "react-icons/hi";
 
 const DonationPage = () => {
+  const [categoryName, setCategoryName] = useState("");
+  const [categoryData, setCategoryData] = useState([]);
 
   const locationHandler = (e) => {
     const searchValue = e.target.value;
-  }
+  };
 
-
-  // useEffect(() => {
-  //   fetch('api/education')
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       console.log(data)
-  //     })
-  // }, [])
   useEffect(() => {
-    fetch("api/education")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      });
-  }, []);
-
+    // fetch(`api/${categoryName}`)
+    //   .then((res) => res.json())
+    //   .then((data) => setCategoryData(data));
+  }, [categoryName]);
 
   return (
     <>
       <div className="flex justify-between max-w-7xl my-8 mx-auto px-5 lg:px-0">
         <div className="form-control flex-row items-center">
-          <HiFilter className='text-[#9774FF] w-6 h-6 inline' />
-          <h2 className='mr-2'>Filter: </h2>
+          <HiFilter className="text-[#9774FF] w-6 h-6 inline" />
+          <h2 className="mr-2">Filter: </h2>
           <label className="label cursor-pointer justify-normal gap-2">
-            <input type="radio" value="seller" className="radio checked:bg-red-500" checked />
+            <input
+              type="radio"
+              value="seller"
+              className="radio checked:bg-red-500"
+              checked
+            />
             <span className="label-text">All</span>
           </label>
 
           <label className="label cursor-pointer justify-normal gap-2">
-            <input type="radio" value="buyer" className="radio checked:bg-blue-500" checked />
+            <input
+              type="radio"
+              value="buyer"
+              className="radio checked:bg-blue-500"
+              checked
+            />
             <span className="label-text">Emergency</span>
           </label>
         </div>
 
         <div className="form-control">
-          <select onChange={locationHandler} defaultValue='Location' name='category' className="space-y-5 select w-full max-w-xs input input-bordered">
-            <option >Location</option>
+          <select
+            onChange={locationHandler}
+            defaultValue="Location"
+            name="category"
+            className="space-y-5 select w-full max-w-xs input input-bordered"
+          >
+            <option>Location</option>
             <option value="India">India</option>
             <option value="Bangladesh">Bangladesh</option>
             <option value="USA">USA</option>
@@ -54,16 +60,37 @@ const DonationPage = () => {
         </div>
       </div>
 
-      <div className='my-4 flex justify-center gap-3'>
-        <Link href='/' className="my_button">Medical</Link>
-        <Link href='/a' className="my_button">Starvation</Link>
-        <Link href='/signup' className="my_button">Education</Link>
-        <Link href='/signup' className="my_button">Orphanage</Link>
-        <Link href='/signup' className="my_button">Oldage</Link>
+      <div className="my-4 flex justify-center gap-3">
+        <button
+          onClick={() => setCategoryName("medical")}
+          className="my_button"
+        >
+          Medical
+        </button>
+        <button
+          onClick={() => setCategoryName("starvation")}
+          className="my_button"
+        >
+          Starvation
+        </button>
+        <button
+          onClick={() => setCategoryName("education")}
+          className="my_button"
+        >
+          Education
+        </button>
+        <button
+          onClick={() => setCategoryName("orphanage")}
+          className="my_button"
+        >
+          Orphanage
+        </button>
+        <button onClick={() => setCategoryName("oldage")} className="my_button">
+          Oldage
+        </button>
       </div>
     </>
+  );
+};
 
-  )
-}
-
-export default DonationPage
+export default DonationPage;

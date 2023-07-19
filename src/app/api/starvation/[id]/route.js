3a71c77f96a9ebd21c785176de2b81a8b2164640
shaikat17 <shaikatpal56@gmail.com>
2023-07-20 00:50:03 +0600

    Orphanage route for single data added.

---
+++ src/app/api/starvation/[id]/route.js
@@ -0,0 +1,23 @@
+import connectMongoDB from "@/config/mongodb";
+import Oldage from "@/models/oldageModel";
+import Starvation from "@/models/starvationModel";
+import { NextResponse } from "next/server";
+
+
+export const GET = async (request, { params }) => {
+    const { id } = params;
+
+    // console.log(id)
+  
+    try {
+      await connectMongoDB();
+  
+      const singleData = await Starvation.findById(id);
+    //   console.log(singleData)
+  
+      return new NextResponse(JSON.stringify(singleData), { status: 200 });
+    } catch (err) {
+      return new NextResponse("Database Error", { status: 500 });
+    }
+    // return NextResponse.json({msg: "Route Successfull"})
+  };
